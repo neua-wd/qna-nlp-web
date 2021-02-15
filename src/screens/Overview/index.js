89f1c421e1757d8237883417937f6b1f52085993
neua-wd <neua.worabodee@gmail.com>
2021-02-15 15:29:01 +0000

    Overview screen

---
+++ src/screens/Overview/index.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Question from '../../components/Question';
+import Choices from '../../components/Choices';
+import Explanation from '../../components/Explanation';
+
+// The Overview screen contains the overview of a particular question
+// Includes the question the choices and the explanation
+const Overview = () => {
+  useEffect(() => {
+    getOverview();
+  }, []);
+
+  const [overview, setOverview] = useState([]);
+
+  const getOverview = async () => {
+    const res = await axios.get('/overview');
+    console.log('getOverview');
+    console.log(res);
+    setOverview(res.data);
+  };
+
+  return (
+    <div>
+      <Question question={overview.question} />
+      <Choices choices={overview.choices} answer={overview.answer} />
+      <Explanation facts={overview.explanation} />
+    </div>
+  );
+};
+
+export default Overview;
