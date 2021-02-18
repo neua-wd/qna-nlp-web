b430c9f8dce0c4ec1a4664898395345212f571fb
neua-wd <neua.worabodee@gmail.com>
2021-02-18 15:26:06 +0000

    Change choice to link on hover

--- src/components/Choices/Choice/index.js
+++ src/components/Choices/Choice/index.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import '../../../styles/components/choice.scss';
 
 // A Choice component includes the letter (ie. A/B/C/D) and the sentence
 const Choice = ({ letter, sentence, correct }) => {
-  return (
-    <div className={`choice choice--${correct ? 'correct' : 'incorrect'}`}>
-      <div className="choice__title">{letter}</div>
-      <br />
-      {sentence}
-    </div>
-  );
+  const [hovered, setHovered] = useState(false);
+
+  const handleHover = () => {
+    setHovered(!hovered);
+  };
+
+  if (!hovered) {
+    return (
+      <div
+        className={`choice choice--${correct ? 'correct' : 'incorrect'}`}
+        onMouseEnter={handleHover}
+      >
+        <div className="choice__title">{letter}</div>
+        <br />
+        {sentence}
+      </div>
+    );
+  } else {
+    return (
+      <div className="choice choice__see-details" onMouseLeave={handleHover}>
+        <div className="choice__see-details--text">See Details ...</div>
+      </div>
+    );
+  }
 };
 
 export default Choice;
