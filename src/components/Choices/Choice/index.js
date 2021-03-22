35ef9dd9b6d52245404d301241506ca824d7d9b8
neua-wd <neua.worabodee@gmail.com>
2021-03-22 19:34:10 +0000

    Remove redundant prop

--- src/components/Choices/Choice/index.js
+++ src/components/Choices/Choice/index.js
@@ -1,7 +1,7 @@
 import '../../../styles/components/choice.scss';
 
 // A Choice component includes the letter (ie. A/B/C/D) and the sentence
-const Choice = ({ letter, sentence, correct, overview, setOverview }) => {
+const Choice = ({ letter, sentence, overview, setOverview }) => {
   const handleClick = letter => {
     const explanation =
       letter == overview.answer ? 'explanation' : 'explanation' + letter;
