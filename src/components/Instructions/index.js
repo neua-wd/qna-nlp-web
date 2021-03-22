ad2ed38d17dc771da6c10f24b9732a6a0410b6f0
neua-wd <neua.worabodee@gmail.com>
2021-03-22 19:33:38 +0000

    Update appearance

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -1,18 +1,23 @@
 import Search from '../Actions/Search';
 import Random from '../Actions/Random';
+import Card from '@material-ui/core/Card';
+
+import '../../styles/components/instructions.scss';
 
 const Instructions = ({ getOverview }) => {
   return (
-    <div>
-      Please search for an exact question <br />
-      (eg. What remains in the same location in the sky of the Northern
-      Hemisphere each night?) <br />
-      <br />
-      <Search getOverview={getOverview} withForm={true} withDesc={false} />
-      <br />
-      or <br />
-      Get a random question
-      <Random getOverview={getOverview} withDesc={false} />
+    <div className="instructions">
+      <Card className="instructions__card">
+        Please search for an exact question <br />
+        (eg. What remains in the same location in the sky of the Northern
+        Hemisphere each night?) <br />
+        <br />
+        <Search getOverview={getOverview} withForm={true} withDesc={false} />
+      </Card>
+      <Card className="instructions__card">
+        Or get a random question
+        <Random getOverview={getOverview} withDesc={false} />
+      </Card>
     </div>
   );
 };
